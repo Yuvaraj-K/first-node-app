7d2ef289c7f7e823240d1872a651b5624346a4fe
Yuvaraj-K <yuvikaruna@gmail.com>
2018-07-19 01:20:54 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log')
+        }
+    });
+    next();
+});
+app.use((req, res,next) => {
+    res.render('maintenance.hbs');
+})
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (message) => {
+    return message.toUpperCase();
+});
+
+app.use(express.static(__dirname + '/public'
+    // , { extensions: ['html']}
+));
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Welcome',
+        message: 'Handlebar intergration works'
+    });
+}).get('/about', (req, res) => {
+    // res.send('<h1>About us</h1>');
+    res.render('about.hbs', {
+        pageTitle: 'About'
+    })
+}).get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server running in port 3000');
+});
