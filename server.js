ce3f7a62ef1ada560adc41faa99d23ba236f8433
Yuvaraj-K <yuvikaruna@gmail.com>
2018-07-21 14:14:21 +0530

    projects page added

--- server.js
+++ server.js
@@ -47,6 +47,10 @@ app.get('/', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About'
     })
+}).get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Project'
+    })
 }).get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
